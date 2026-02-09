222146f46d9295ee38d3a6db95b7d8b9f917d216
KouroshBeheshtinejad <Kouroshbeheshtinejad@gmail.com>
2026-02-09 11:36:16 +0330

    Rename the Blog installed app for better perfom

--- First_Site/settings.py
+++ First_Site/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "First_App.apps.FirstAppConfig",
-    "Blog"
+    "Blog.apps.BlogConfig"
 ]
 
 MIDDLEWARE = [
