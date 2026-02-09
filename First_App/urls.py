5df85b8eb31b7e753d167f3bbc69d452f6a59766
KouroshBeheshtinejad <Kouroshbeheshtinejad@gmail.com>
2026-02-09 12:41:46 +0330

    Cleaning Code Phase one

--- First_App/urls.py
+++ First_App/urls.py
@@ -6,6 +6,5 @@ app_name = 'First_App'
 urlpatterns = [
     path('', index_view,name='index'),
     path('about', about_view,name='about'),
-    path('contact', contact_view,name='contact')
-    
+    path('contact', contact_view,name='contact'),
 ]
