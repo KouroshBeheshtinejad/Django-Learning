94fd4d20ac4df44b452d7f9052f3d05205f369a0
KouroshBeheshtinejad <Kouroshbeheshtinejad@gmail.com>
2026-02-12 00:01:55 +0330

    customize the tables and admin module

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from Blog.models import Post
 # Register your models here.
+# @admin.register(Post)
+class PostAmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    # fields = ('title',)
+    list_display = ('title', 'status', 'counted_view')
+    list_filter = ('status',)
+    # ordering = ['created_date']
+    search_fields = ['title', 'content']
+admin.site.register(Post, PostAmin)
