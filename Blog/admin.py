f83f7d8566d9f086faf2cf329c564b6023a735c6
KouroshBeheshtinejad <Kouroshbeheshtinejad@gmail.com>
2026-02-13 22:00:11 +0330

    adding author fileds to the Blog application

--- Blog/admin.py
+++ Blog/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from Blog.models import Post
 # Register your models here.
 # @admin.register(Post)
-class PostAmin(admin.ModelAdmin):
+class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
     # fields = ('title',)
-    list_display = ('id','title', 'status', 'counted_view')
-    list_filter = ('status',)
+    list_display = ('id','title', 'author', 'status', 'counted_view')
+    list_filter = ('status', 'author',)
     # ordering = ['created_date']
     search_fields = ['title', 'content']
-admin.site.register(Post, PostAmin)
+admin.site.register(Post, PostAdmin)
--- Blog/models.py
+++ Blog/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Post(models.Model):
     # image
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     # tag
     # category
-    # author
     counted_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
