555bd93a283a2b99928d8fefdcd676a54dcb2b42
KouroshBeheshtinejad <Kouroshbeheshtinejad@gmail.com>
2026-02-14 14:53:21 +0330

    adding image and Category filed to Blog App

--- Blog/models.py
+++ Blog/models.py
@@ -2,15 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
-    # image
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)  # or author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
     # tag
-    # category
+    category = models.ManyToManyField(Category)
     counted_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
@@ -21,7 +26,6 @@ class Post(models.Model):
         ordering = ['-created_date']
         # verbose_name = 'پست'
         # verbose_name_plural = 'پست ها'
-    
     
     def __str__(self):
         return "{} - {}".format(self.title, self.id)
