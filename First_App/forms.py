c538657a9bf91b7bd7fcef6df3123ee19daec3ca
KouroshBeheshtinejad <Kouroshbeheshtinejad@gmail.com>
2026-02-16 22:55:30 +0330

    adding newsletter subscription form and messages

---
+++ First_App/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from First_App.models import Contact, Newsletter
+
+class NameForm(forms.Form):
+    name = forms.CharField(label='Your name', max_length=255)
+    email = forms.EmailField(label='Your email', max_length=255)
+    subject = forms.CharField(label='Subject', max_length=255)
+    message = forms.CharField(label='Message', widget=forms.Textarea)
+    
+    
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['subject'].required = False
+        
+class NewsletterForm(forms.ModelForm):
+    class Meta:
+        model = Newsletter
+        fields = '__all__'
--- First_App/views.py
+++ First_App/views.py
@@ -1,16 +1,57 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from Blog.models import Post
+from First_App.models import Contact, Newsletter  
+from First_App.forms import ContactForm, NewsletterForm
+from django.contrib import messages
 
 def index_view(request):
     latest_posts = Post.objects.filter(status=1).order_by('-published_date')[:6]
     context = {'latest_posts': latest_posts}
     return render(request, 'website/index.html', context)
 
+
 def about_view(request):
     return render(request, 'website/about.html')
 
+
 def contact_view(request):
-    return render(request, 'website/contact.html')
+    if request.method == 'POST':
+        post_data = request.POST.copy()
+        post_data['name'] = 'ناشناس'
+        form = ContactForm(post_data)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent successfully!')
+        else:
+            messages.error(request, 'There was an error sending your message. Please try again.')
+    form = ContactForm()
+    context = {'form': form}
+    return render(request, 'website/contact.html', context)
+
 
 def elements_view(request):
     return render(request, 'website/elements.html')
+
+
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('/')
+
+
+def test_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Form submitted successfully!')
+        elif form.errors:
+            return HttpResponse(f'Form errors: {form.errors}')
+    form = ContactForm()
+    context = {'form': form}
+    return render(request, 'test.html', context)
