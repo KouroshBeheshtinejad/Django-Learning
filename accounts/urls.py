8aaa307503a64818c96b2295c8f8d276ca2dc5b7
KouroshBeheshtinejad <Kouroshbeheshtinejad@gmail.com>
2026-02-22 23:03:25 +0330

    Create a new Django app called "accounts" and implement user authentication views for login, logout, and signup. Update the project's URL configuration to include the accounts app URLs. Create corresponding templates for each view to render the forms and display messages to the user.

---
+++ accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from accounts.views import login_view, logout_view, signup_view
+
+
+app_name = 'accounts'
+
+urlpatterns = [
+    # login
+    path('login/', login_view, name='login'),
+    # logout
+    path('logout/', logout_view, name='logout'),
+    # register / sign up
+    path('signup/', signup_view, name='signup'),
+]
