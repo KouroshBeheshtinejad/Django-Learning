989ec372ecf1611b6b675365aa3c1774d4ad2925
KouroshBeheshtinejad <Kouroshbeheshtinejad@gmail.com>
2026-02-23 17:35:53 +0330

    Getting ready to project for deploying and build the dev and prod file for same reason

---
+++ First_Site/setting/dev.py
@@ -0,0 +1,34 @@
+from First_Site.settings import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-%@klb+_g*aixg!j7js_(1ib&ntu)%n3khtp*kb8o6m65_ekz(e"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# INSTALLED_APPS = []
+
+# sites framework
+SITE_ID = 2
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
--- First_Site/settings.py
+++ First_Site/settings.py
@@ -16,16 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%@klb+_g*aixg!j7js_(1ib&ntu)%n3khtp*kb8o6m65_ekz(e"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -51,8 +42,6 @@ INSTALLED_APPS = [
     "Blog.apps.BlogConfig",
     "accounts.apps.AccountsConfig",
 ]
-
-SITE_ID = 2
 
 ROBOTS_USE_HOST = False
 ROBOTS_USE_SITEMAP = False
@@ -125,16 +114,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "First_Site.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -171,14 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "statics",
-]
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
